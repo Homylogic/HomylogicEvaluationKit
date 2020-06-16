6bf3dbaa60d966f103c86c826eaf84593e98dc69
Homylogic <homylogic@gmail.com>
2020-06-16 13:25:59 +0200

    New version July/2020 Beta.

--- X.Homylogic/Body.cs
+++ X.Homylogic/Body.cs
@@ -14,7 +14,7 @@ namespace X.Homylogic
 {
     public static class Body
     {
-        public const string VERSION_NAME = "July/2020";
+        public const string VERSION_NAME = "June/2020 Beta";
 
         public static Models.Database Database { get; private set; }
         public static Models.Environment Environment { get; private set; }
